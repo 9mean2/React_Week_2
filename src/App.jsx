e709e49add9ab513b78209ac4fbe041e62188927
9mean2 <o3omoomin@gmail.com>
2023-02-12 21:47:15 +0900

    feat: React Hooks - useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <div>
-      <span>스타일 테스트</span>
-      <h1>h1인데요</h1>
-      <p>나는 p 태그입니다</p>
-    </div>
+    <>
+      <div>Number State : {number}</div>;
+      <button
+        onClick={() => {
+          //기존 업데이트
+          //배치성으로 처리된다 , 배치 업데이트.
+
+          setNumber(number + 1);
+          setNumber(number + 1);
+          setNumber(number + 1);
+
+          // 렌더링이 잦다 -> 성능에 이슈가 있는 것 !
+
+          // 함수형 업데이트
+
+          setNumber((currentNumber) => currentNumber + 1);
+          setNumber((currentNumber) => currentNumber + 1);
+          setNumber((currentNumber) => currentNumber + 1);
+        }}
+      >
+        {" "}
+        누르면up
+      </button>
+    </>
   );
 }
 
