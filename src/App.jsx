2a1d58f12eba2e010317587b40a6c3bbf40a626d
9mean2 <o3omoomin@gmail.com>
2023-02-13 04:00:32 +0900

    feat:  React Hooks - state와 ref의 차이점

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+
+const style = {
+  border: "1px solid black",
+  margin: "10px",
+  padding: "10px",
+};
 
 function App() {
-  //ref : reference
-  const ref = useRef("초기값");
-  console.log("ref", ref);
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
 
-  ref.current = "변경값";
-  console.log("ref2", ref);
+  // 화면에 렌더링이 계속 일어나고 있다
+  const plusStateCountBtn = () => {
+    setCount(count + 1);
+    console.log(1);
+  };
 
-  return <div>App</div>;
+  const plusRefCountBtn = () => {
+    countRef.current++;
+    console.log(countRef.current);
+  };
+
+  return (
+    <>
+      <div style={style}>
+        state 영역입니다. {count} <br />
+        <button onClick={plusStateCountBtn}> state 증가 </button>
+      </div>
+      <div style={style}>
+        ref 영역입니다 {countRef.current} <br />
+        <button onClick={plusRefCountBtn}>ref 증가</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
