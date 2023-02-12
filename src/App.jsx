865389256fcdbf2e5821bee57c11e99404797bfe
9mean2 <o3omoomin@gmail.com>
2023-02-13 04:17:30 +0900

    feat:  React Hooks - 예제 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
   const idRef = useRef("");
+  const pwRef = useRef("");
+
+  const [id, setId] = useState("");
 
   //화면이 렌더링 될 때 어떤 작업을 하고 싶다 : useEffect
 
   useEffect(() => {
-    idRef.current.focus();
+    // idRef.current.focus();
   }, []);
+
+  useEffect(() => {
+    if (id.length >= 10) {
+      pwRef.current.focus();
+    }
+  }, [id]);
 
   return (
     <>
       <div>
-        아이디 : <input type="text" ref={idRef} />{" "}
+        아이디 :{" "}
+        <input
+          value={id}
+          onChange={(event) => {
+            setId(event.target.value);
+          }}
+          type="text"
+        />{" "}
       </div>
       <div>
-        비밀번호 : <input type="password" />
+        비밀번호 : <input ref={pwRef} type="password" />
       </div>
     </>
   );
