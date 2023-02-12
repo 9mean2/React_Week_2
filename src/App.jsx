e06a7589f376e6f27703abb8cd375e92a775f0ab
9mean2 <o3omoomin@gmail.com>
2023-02-12 21:55:07 +0900

    feat: push test

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
           //기존 업데이트
           //배치성으로 처리된다 , 배치 업데이트.
 
-          setNumber(number + 1);
-          setNumber(number + 1);
-          setNumber(number + 1);
+          // setNumber(number + 1);
+          // setNumber(number + 1);
+          // setNumber(number + 1);
 
           // 렌더링이 잦다 -> 성능에 이슈가 있는 것 !
 
