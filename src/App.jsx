b40255add400bc832387920387070bdd914be730
9mean2 <o3omoomin@gmail.com>
2023-02-13 03:45:28 +0900

    feat:  React Hooks - useRef - 기본 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
 
 function App() {
-  const [value, setvalue] = useState("");
+  //ref : reference
+  const ref = useRef("초기값");
+  console.log("ref", ref);
 
-  // clean up
-  useEffect(() => {
-    console.log(`hello world : ${value}`);
+  ref.current = "변경값";
+  console.log("ref2", ref);
 
-    // return () => {
-    //   //
-    //   console.log("바이바이");
-    // };
-  }, [value]);
-
-  return (
-    <div>
-      {/* 1. input에 값을 입력 */
-      /* 2. value, 즉 state가 변경 */
-      /* 3. state가 바뀌었기 때문에 -> App 컴포넌트가 리렌더링
-      4. 리렌더링 -> useEffect()
-      5. 1~4가 반복 */
-      /* Dependency Array} */}
-
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => {
-          setvalue(event.target.value);
-        }}
-      />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
