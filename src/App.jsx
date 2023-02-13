f0aaf9dbe5c807ab4038baa10b933f6d68d04d8e
9mean2 <o3omoomin@gmail.com>
2023-02-13 16:49:04 +0900

    feat: 07. React Hooks - useMemo 사용하기 전

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
-import React, { useCallback, useState } from "react";
-import Box1 from "./components/Box1";
-import Box2 from "./components/Box2";
-import Box3 from "./components/Box3";
+import React from "react";
+import HavyComponents from "./components/HavyComponents";
 
+// heavy work -> 엄청 거운 작업
 function App() {
-  console.log("app컴포 렌더링됐음");
-  const [count, setCount] = useState(0);
-
-  // 1 증가
-  const onPlusBtnClickHandler = () => {
-    setCount(count + 1);
-  };
-
-  // 1 감소
-  const onMinusBtnClickHandler = () => {
-    setCount(count - 1);
-  };
-
-  // counst 초기화해주는 함수
-
-  const initCount = useCallback(() => {
-    console.log(`${count}에서 0으로 변경되었습니다.`);
-    setCount(0);
-  }, [count]);
-
   return (
     <>
-      <h3>카운트 예제입니다! ! ! </h3>
-      <p>현재 카운트 : {count}</p>
-      <button onClick={onPlusBtnClickHandler}> + </button>
-      <button onClick={onMinusBtnClickHandler}> - </button>
-      <div
+      <nav
         style={{
-          display: "flex",
-          marginTop: "10px",
+          backgroundColor: "yellow",
+          marginBottom: "30px",
         }}
       >
-        <Box1 initCount={initCount} />
-        <Box2 />
-        <Box3 />
-      </div>
+        네비게이션 바
+      </nav>
+      <HavyComponents />
+      <footers
+        style={{
+          backgroundColor: "green",
+        }}
+        ssss
+      >
+        푸터 영역이에요
+      </footers>
     </>
   );
 }
