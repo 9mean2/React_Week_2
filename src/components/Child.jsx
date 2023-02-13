4c739c5ec52a644d73633c1fe1e5718d3e307370
9mean2 <o3omoomin@gmail.com>
2023-02-13 15:06:56 +0900

    feat: React Hooks - useContext 사용

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FamilyContext } from "../context/FamiltContext";
 
 const style = {
   color: "red",
   fontWeight: "900",
 };
 
-function Child({ houseName, pocketMoney }) {
+function Child() {
+  const data = useContext(FamilyContext);
+  console.log("data", data);
   return (
     <div>
-      나는 이 집안의 막내에요 <br />
-      할아버지가 우리집 이름은 <span style={style}>{houseName}</span> 이라고
-      하셨어요 <br />
-      게다가 용돈도 <span style={style}>{pocketMoney}</span>원 만큼 주셨어요
+      나는 이 집안의 막내에요 응애~ <br />
+      할아버지가 우리집 이름은 <span style={style}>{data.houseName}</span>{" "}
+      이라고 하셨어요 <br />
+      게다가 용돈도 <span style={style}>{data.pocketMoney}</span>원 만큼
+      주셨어요
     </div>
   );
 }
--- src/components/GrandFather.jsx
+++ src/components/GrandFather.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { FamilyContext } from "../context/FamiltContext";
 import Father from "./Father";
 // GF > Child 한테 어떤 정보를 알려줘서 Child가 그 내용을 출력하도록
 function GrandFather() {
   const houseName = "스파르타";
   const pocketMoney = 10000;
-  return <Father houseName={houseName} pocketMoney={pocketMoney} />;
+  return (
+    <FamilyContext.Provider
+      value={{
+        houseName: houseName,
+        pocketMoney: pocketMoney,
+      }}
+    >
+      <Father />;
+    </FamilyContext.Provider>
+  );
 }
 
 export default GrandFather;
