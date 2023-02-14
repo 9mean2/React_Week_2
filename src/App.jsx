418a8f178a885a1173f6e8f5cde7ec0c171c6e37
9mean2 <o3omoomin@gmail.com>
2023-02-14 16:22:31 +0900

    Redux Payload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { plusOne, minusOne } from "./redux/modules/counter";
-
+import { plusN } from "./redux/modules/counter";
+import { minusN } from "./redux/modules/counter";
 function App() {
+  const [number, setNumber] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("number -> " + number);
+  // }, []);
+
   // 여기에서 store에 접근하여, counter의 값을 읽어오고 싶다
   // useSelector
 
@@ -15,18 +22,27 @@ function App() {
 
   const dispatch = useDispatch();
 
-  console.log("counter ->", counter.number);
-
   return (
     <>
       <div>현재 카운트 : {counter.number}</div>;
+      <div>
+        <input
+          type="number"
+          value={number}
+          onChange={(event) => {
+            const { value } = event.target;
+            setNumber(+value);
+          }}
+        />
+      </div>
       <button
         onClick={() => {
           // // +1 을 해주는 로직을 써주면 된다
           // dispatch({
           //   type: PLUS_ONE,
           // });
-          dispatch(plusOne());
+          // dispatch(plusOne());
+          dispatch(plusN(number));
         }}
       >
         +
@@ -37,10 +53,10 @@ function App() {
           // dispatch({
           //   type: MINUS_ONE,
           // });
-          dispatch(minusOne());
+          dispatch(minusN(number));
         }}
       >
-        +
+        -
       </button>
     </>
   );
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -2,6 +2,8 @@
 
 export const PLUS_ONE = "PLUS_ONE";
 export const MINUS_ONE = "MINUS_ONE";
+export const PLUS_N = "PLUS_N";
+export const MINUS_N = " MINUS_N";
 
 // action creator  : action value 를 return 하는 함수
 
@@ -17,12 +19,30 @@ export const minusOne = () => {
   };
 };
 
+export const plusN = (payload) => {
+  return {
+    type: PLUS_N,
+    payload: payload,
+  };
+};
+
+export const minusN = (payload) => {
+  return {
+    type: MINUS_N,
+    payload: payload,
+  };
+};
+
 // 초기 상태값
 const initialState = {
   number: 0,
 };
 
 // 리듀서
+
+// 액션 객체라는 것은 액션 타입을 페이로드 만큼 처리하는 것이다
+// 페이로드가 3이다. 3만큼을 플러스
+
 const counter = (state = initialState, action) => {
   switch (action.type) {
     case PLUS_ONE:
@@ -32,6 +52,14 @@ const counter = (state = initialState, action) => {
     case MINUS_ONE:
       return {
         number: state.number - 1,
+      };
+    case PLUS_N:
+      return {
+        number: state.number + action.payload,
+      };
+    case MINUS_N:
+      return {
+        number: state.number - action.payload,
       };
     default:
       return state;
