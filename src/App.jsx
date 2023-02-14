e02f11472094f8bf4b117109380b066e8d913bb7
9mean2 <o3omoomin@gmail.com>
2023-02-14 15:55:19 +0900

    Redux action Value Creator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { plusOne, minusOne } from "./redux/modules/counter";
 
 function App() {
   // 여기에서 store에 접근하여, counter의 값을 읽어오고 싶다
@@ -21,10 +22,11 @@ function App() {
       <div>현재 카운트 : {counter.number}</div>;
       <button
         onClick={() => {
-          // +1 을 해주는 로직을 써주면 된다
-          dispatch({
-            type: "PLUS_ONE",
-          });
+          // // +1 을 해주는 로직을 써주면 된다
+          // dispatch({
+          //   type: PLUS_ONE,
+          // });
+          dispatch(plusOne());
         }}
       >
         +
@@ -32,9 +34,10 @@ function App() {
       <button
         onClick={() => {
           // +1 을 해주는 로직을 써주면 된다
-          dispatch({
-            type: "MINUS_ONE",
-          });
+          // dispatch({
+          //   type: MINUS_ONE,
+          // });
+          dispatch(minusOne());
         }}
       >
         +
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,5 +1,22 @@
 // src/modules/counter.js
 
+export const PLUS_ONE = "PLUS_ONE";
+export const MINUS_ONE = "MINUS_ONE";
+
+// action creator  : action value 를 return 하는 함수
+
+export const plusOne = () => {
+  return {
+    type: PLUS_ONE,
+  };
+};
+
+export const minusOne = () => {
+  return {
+    type: MINUS_ONE,
+  };
+};
+
 // 초기 상태값
 const initialState = {
   number: 0,
@@ -8,11 +25,11 @@ const initialState = {
 // 리듀서
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case "PLUS_ONE":
+    case PLUS_ONE:
       return {
         number: state.number + 1,
       };
-    case "MINUS_ONE":
+    case MINUS_ONE:
       return {
         number: state.number - 1,
       };
